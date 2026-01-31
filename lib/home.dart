42b732100254509810c09f50deebcd44fa35ddb9
Nikie <nikcastro2006@gmail.com>
2026-01-30 22:10:06 -0400

    Add `google_fonts` dependencies, and implement the initial calculator UI.

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Calculator extends StatelessWidget {
   const Calculator({super.key});
@@ -6,11 +7,89 @@ class Calculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-        body: SafeArea(
-          child: Column(children: [Container(color: Color(0xffEFCAD2))]),
+        appBar: AppBar(
+          title: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Calculator',
+                    style: GoogleFonts.jersey15(
+                      textStyle: TextStyle(fontSize: 40),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
+        // direciona pro body da calculadora
+        body: CalculatorBody(),
+      ),
+    );
+  }
+}
+
+// body da calculadora
+class CalculatorBody extends StatefulWidget {
+  const CalculatorBody({super.key});
+
+  @override
+  State<CalculatorBody> createState() => _CalculatorBodyState();
+}
+
+class _CalculatorBodyState extends State<CalculatorBody> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'C',
+                    style: GoogleFonts.jersey15(
+                      textStyle: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'E',
+                    style: GoogleFonts.jersey15(
+                      textStyle: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    '=',
+                    style: GoogleFonts.jersey15(
+                      textStyle: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(child: Text('1')),
+              Expanded(child: Text('2')),
+              Expanded(child: Text('3')),
+            ],
+          ),
+        ],
       ),
     );
   }
